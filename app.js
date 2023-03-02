8c6e76a134843588db94fe7e54a9768790ef4f2c
Pavel Alekseev <pavelbooking.com@yandex.ru>
2023-03-02 11:12:56 +0500

    middlewares + fixed some bugs

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const { errors } = require('celebrate');
+const bodyParser = require('body-parser');
+const cors = require('./middlewares/cors');
+const { requestLogger } = require('./middlewares/logger');
+const router = require('./routes/index');
+const { errorLogger } = require('./middlewares/logger');
+const handleError = require('./middlewares/handleError');
 
 const app = express();
 
@@ -20,3 +27,15 @@ mongoose
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
 });
+
+app.use(cors);
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(requestLogger);
+
+app.use(router);
+
+router.use(errorLogger);
+router.use(errors());
+router.use((err, req, res, next) => { handleError(err, res, next); });
--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,4 @@
-const routerCommon = require('express').Router();
+const router = require('express').Router();
 const { celebrate, Joi } = require('celebrate');
 const auth = require('../middlewares/auth');
 const { login, createUser } = require('../controllers/user');
@@ -6,7 +6,7 @@ const NotFound = require('../errors/NotFound');
 const routerUsers = require('./user');
 const routerMovies = require('./movie');
 
-routerCommon.post('/signup', celebrate({
+router.post('/signup', celebrate({
   body: Joi.object().keys({
     name: Joi.string().required().min(2).max(30),
     email: Joi.string().required().email(),
@@ -14,18 +14,18 @@ routerCommon.post('/signup', celebrate({
   }),
 }), createUser);
 
-routerCommon.post('/signin', celebrate({
+router.post('/signin', celebrate({
   body: Joi.object().keys({
     email: Joi.string().required().email(),
     password: Joi.string().required(),
   }),
 }), login);
 
-routerCommon.use('/users', auth, routerUsers);
-routerCommon.use('/movies', auth, routerMovies);
+router.use('/users', auth, routerUsers);
+router.use('/movies', auth, routerMovies);
 
-routerCommon.use('*', auth, (req, res, next) => {
+router.use('*', auth, (req, res, next) => {
   next(new NotFound('Маршрут не найден'));
 });
 
-module.exports = routerCommon;
+module.exports = router;
